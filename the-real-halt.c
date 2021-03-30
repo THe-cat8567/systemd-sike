b5ab0011797e39cbe4a30784e285b9704581d784
THe-cat8567 <wow-cool2781@protonmail.com>
2021-03-29 20:21:37 -0700

    halt as well

---
+++ the-real-halt.c
@@ -0,0 +1,12 @@
+#include <sys/reboot.h>
+
+int
+main(int argc, char *argv[])
+{
+    switch ((int) argv[argc < 2 ? 0 : 1][0]) {
+    case 'p': reboot(RB_POWER_OFF); break;
+    case 'r': reboot(RB_AUTOBOOT);  break;
+    default: return 1;
+    }
+    return 0;
+}
